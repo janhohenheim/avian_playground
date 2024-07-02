3fa1b45d4f43918dc6ad4e151f3b40ad644b94a6
Jan Hohenheim <jan@hohenheim.ch>
2024-07-02 12:19:33 +0200

    Make bug more reproducible

--- src/bin/joint.rs
+++ src/bin/joint.rs
@@ -51,11 +51,13 @@ fn setup(
 
     let body_shape = Cuboid::new(1.0, 1.0, 1.0);
     let lid_shape = Cuboid::new(1.0, 0.1, 1.0);
+    let joint_shape = Sphere::new(0.1);
 
     let body_mesh = meshes.add(Mesh::from(body_shape));
     let lid_mesh = meshes.add(Mesh::from(lid_shape));
+    let joint_mesh = meshes.add(Mesh::from(joint_shape));
 
-    let y_offset = 0.3;
+    let y_offset = 0.1;
     let body_joint_anchor = Vec3::new(-0.5, 0.5 + y_offset, 0.0);
     let lid_joint_anchor = Vec3::new(0.0, -0.05 - y_offset, 0.5);
 
@@ -71,6 +73,17 @@ fn setup(
             RigidBody::Static,
             Collider::from(body_shape),
         ))
+        .with_children(|parent| {
+            parent.spawn((
+                Name::new("Body Joint Visualizer"),
+                PbrBundle {
+                    mesh: joint_mesh.clone(),
+                    material: white.clone(),
+                    transform: Transform::from_translation(body_joint_anchor),
+                    ..default()
+                },
+            ));
+        })
         .id();
 
     let lid = commands
@@ -79,19 +92,30 @@ fn setup(
             PbrBundle {
                 mesh: lid_mesh,
                 material: white.clone(),
-                transform: Transform::from_xyz(0.0, 2.5, 0.0)
+                transform: Transform::from_xyz(0.0, 2.2, 0.0)
                     .looking_to(Vec3::new(1.0, 1.0, 0.0), Vec3::Y),
                 ..default()
             },
-            RigidBody::Static,
+            RigidBody::Dynamic,
             Collider::from(lid_shape),
         ))
+        .with_children(|parent| {
+            parent.spawn((
+                Name::new("Lid Joint Visualizer"),
+                PbrBundle {
+                    mesh: joint_mesh,
+                    material: white.clone(),
+                    transform: Transform::from_translation(lid_joint_anchor),
+                    ..default()
+                },
+            ));
+        })
         .id();
 
     commands.spawn((
         Name::new("RevoluteJoint"),
         RevoluteJoint::new(body, lid)
-            .with_aligned_axis(Vec3::X)
+            .with_aligned_axis(Vec3::Z)
             .with_local_anchor_1(body_joint_anchor)
             .with_local_anchor_2(lid_joint_anchor),
     ));
